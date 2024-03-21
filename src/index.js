8a35d8c697db2d3afe3c091b3d8fc3a13c06383f
shalini-itobuz <shalini@itobuz.com>
2024-03-21 12:25:12 +0530

    feat:2 new routes search last 3 , search title

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import express from 'express';
 import mongoose from 'mongoose';
 import routes from './routes/index.js';
 import dotenv from 'dotenv';
+import connectDB from './connect.js';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5500;
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/notes');
+// mongoose.connect('mongodb://localhost:27017/notes');
 
 app.use('/api', routes);
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
